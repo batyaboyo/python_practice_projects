bb5fe8e8dbb2e4f93bfa803e1b807f0a118488d3
devbatx <batztonnie@gmail.com>
2023-10-21 20:00:01 +0300

    updates

--- py_img_editor/main.py
+++ py_img_editor/main.py
@@ -1,18 +1,19 @@
 from PIL import Image, ImageEnhance, ImageFilter
+
 import os
 
 path = "./imgs"
 pathOut = "./edited"
 
 for file in os.listdir(path):
-    img = Image.open("f{path}/{filename}")
+    img = Image.open(f"{path}/{file}")
     
-    edit = img.filter(imageFilter.SHARPEN).convert("L")
+    edit = img.filter(ImageFilter.SHARPEN).convert("L")
     
     factor = 1.5
-    enhancer = ImageEnhancer.contrast(edit)
+    enhancer = ImageEnhance.Contrast(edit)
     edit = enhancer.enhance(factor)
     
-    clean_name = os.path.splitext(filename)[0]
+    clean_name = os.path.splitext(file)[0]
     
-    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
+    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
