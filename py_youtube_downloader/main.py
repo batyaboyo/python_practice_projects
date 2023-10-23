d1fd65ec1d819b450a95050e4537e217058412db
devbatx <batztonnie@gmail.com>
2023-10-23 21:08:51 +0300

    updates

--- py_youtube_downloader/main.py
+++ py_youtube_downloader/main.py
@@ -1,6 +1,10 @@
 from pytube import YouTube
 from sys import argv
 
+if len(argv) < 2:
+    print("Please provide a YouTube link as a command-line argument.")
+    exit()
+
 link = argv[1]
 yt = YouTube[link]
 
